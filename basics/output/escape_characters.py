9dfe4f2896a3527632ad138bbc975bd8c2f4da77
kristofar1996 <>
2021-06-28 20:35:42 +0000

    Updated code with escape_characters

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,7 @@
+# Display escape characters
+print("\n Display a new line")
+print("\t Display a tab space")
+print("\\ Display a back slash")
+print("\" Display a double quote")
+print("\' Display a single quote")
+print(" \tI am Beep!")
--- basics/output/multiline_message.py
+++ basics/output/multiline_message.py
@@ -1,4 +1,9 @@
 #display message in to standart output in multiple lines
 print("System Failure Imminent")
 print("System reboot has been initiated...")
-  
+print()
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
+print()
+print("System onnline.")

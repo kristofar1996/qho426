42d32bc23471aba9319a671b2100c157b3b5013d
kristofar1996 <>
2021-07-01 09:56:35 +0000

    New file String operators for Symbols

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-print("Please enter the number of lives")
-lives = int(input())
 
-print("Please enter the nergy level")
-energy = int(input())
-
-print("Please enter the shield level")
-shield = int(input())
-
-print ("Health has been set!")
-print("Lives : " + lives*"\u2665")
-print("Energy : " + energy*"\u2666")
-print("Shield : " + shield*"\u2666")
-
-
-#how to add symbols
-#print("\u270A" * 3)  prints 3 hands
 
 
 
